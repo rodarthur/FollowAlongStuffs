0140b5a62c51cd52b156afe6d1649f8d23c6a394
rod <rodney@esha.com>
2017-08-11 14:23:44 -0700

    Put in functionality to toggle visiability.

--- vuejs/script.js
+++ vuejs/script.js
@@ -8,16 +8,31 @@
 // };
 
 
-  var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  });
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
 
-  var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date()
-    }
-  });
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date()
+  }
+});
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+});
+
+var x = document.getElementById("app3seenswitch");
+x.addEventListener("click", function() {
+  app3.seen = !app3.seen;
+  // console.log('this is doing something');
+});
+
+// function a(){app3.seen = !app3.seen;console.log('this is doing something');}
