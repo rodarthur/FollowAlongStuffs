468a9db920bd5170e265dd1b0971583304695bcf
rod <rodney@esha.com>
2017-08-11 14:06:20 -0700

    Great job! You managed to get hello world up!

---
+++ vuejs/script.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('load', stuffloaded);
+
+function stuffloaded() {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+
+  
+};
